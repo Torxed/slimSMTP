44bf39e041427d10579932d9ba09e41eec15488e
Anton Hvornum <anton@hvornum.se>
2022-02-21 15:46:00 +0100

    Enhanced log messages

--- slimSMTP/storage/memory.py
+++ slimSMTP/storage/memory.py
@@ -1,5 +1,7 @@
-from typing import TYPE_CHECKING
 import pydantic
+import logging
+from typing import TYPE_CHECKING
+from ..logger import log
 
 if TYPE_CHECKING:
 	from ..sockets import Client
@@ -8,11 +10,14 @@ class Memory(pydantic.BaseModel):
 	transaction_serial_id :int = 0
 
 	def begin_transaction(self, address :str) -> int:
+		log(f"Using dummy storage Memory()", level=logging.WARNING, fg="red")
 		self.transaction_serial_id += 1
 		return self.transaction_serial_id - 1
 
 	def store_email(self, client :'Client') -> bool:
+		log(f"Using dummy storage Memory(), mail has not been stored", level=logging.WARNING, fg="red")
 		return True
 
 	def set_transaction_as_secure(self, transaction_id :int) -> None:
+		log(f"Using dummy storage Memory()", level=logging.WARNING, fg="red")
 		return None
